fa4f30ca26b61a487433f2e20589b8220de1f24b
Kenn4095 <kenn4095@gmail.com>
2023-06-04 23:55:16 -0400

    Plugin works, but fixing is definitely needed

--- src/main/java/com/helmetcheck/HelmetCheckPlugin.java
+++ src/main/java/com/helmetcheck/HelmetCheckPlugin.java
@@ -5,8 +5,12 @@ import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
+import net.runelite.api.Player;
 import net.runelite.api.GameState;
 import net.runelite.api.events.GameStateChanged;
+import net.runelite.api.events.PlayerSpawned;
+import net.runelite.api.events.PlayerChanged;
+import net.runelite.api.kit.KitType;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
@@ -42,7 +46,42 @@ public class HelmetCheckPlugin extends Plugin
 		if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
 		{
 			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Example says " + config.greeting(), null);
+			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Let me check if you're wearing a helmet.", null);
+
 		}
+	}
+
+	@Subscribe
+	public void onPlayerSpawned(PlayerSpawned event)
+	{
+		final Player player = event.getPlayer();
+		if (client.getLocalPlayer().equals(player))
+		{
+			if(!checkHelmet(player)){
+				client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "YOU AREN'T WEARING A HELMET!", null);
+			}else{
+				client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Oh thank goodness, you're wearing a helmet.", null);
+			}
+		}
+	}
+
+	@Subscribe
+	public void onPlayerChanged(PlayerChanged event){
+		final Player player = event.getPlayer();
+		if(client.getLocalPlayer().equals(player)){
+			if(!checkHelmet(player)){
+				client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Why did you take off your helmet?", null);
+			}else{
+				client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Thank you for putting on your helmet!", null);
+			}
+		}
+	}
+
+	private boolean checkHelmet(Player player){
+		if(player.getPlayerComposition().getEquipmentId(KitType.HEAD) == -1){
+			return false;
+		}
+		return true;
 	}
 
 	@Provides
